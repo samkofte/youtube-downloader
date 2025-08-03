b6dfc51b01bcf6db56bc2d1d6ce843d8c4cddf14
Nasıl Oynanmaz ¿ <tasmertsamet@gmail.com>
2025-08-03 23:53:52 +0300

    Replace @distube/ytdl-core with ytdl-core to fix module issues

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ytdl = require('@distube/ytdl-core');
+const ytdl = require('ytdl-core');
 const youtubedl = require('youtube-dl-exec');
 const cors = require('cors');
 const path = require('path');
